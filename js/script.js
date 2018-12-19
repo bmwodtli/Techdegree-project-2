0eff852e2f0aef3bad5972b7854ba53fcec3c9ba
bmwodtli <45266770+bmwodtli@users.noreply.github.com>
2018-12-19 10:39:48 -0800

    remove active class correction

--- js/script.js
+++ js/script.js
@@ -3,11 +3,8 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 ******************************************/
 
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-const studentList = document.querySelectorAll('.student-list');
+// select student-item and store in variable
 const studentItem = document.querySelectorAll('.student-item');
-const studentDetails = document.querySelectorAll('.student-details');
 let firstPage = 1;
 const studentsPerPage = 10;
 
@@ -32,25 +29,24 @@ for(let i =0;i<list.length;i++){
 
 // define appendPageLinks function
 const appendPageLinks = (list)=>{
+ // determine how many pages are needed
+ let numOfPages = Math.ceil(studentItem.length/studentsPerPage);
 
-// determine how many pages are needed
-let numOfPages = Math.ceil(studentItem.length/studentsPerPage);
+ //create a div, give it the "pagination" class. and append to .page div
+ const pageDiv = document.querySelector('.page');
+ let newDiv = document.createElement('div');
+ newDiv.className = "pagination";
+ pageDiv.appendChild(newDiv);
+ let refNode = document.querySelector('ul');
+ refNode.after(newDiv);
 
-//create a div, give it the "pagination" class. and append to .page div
-const pageDiv = document.querySelector('.page');
-let newDiv = document.createElement('div');
-newDiv.className = "pagination";
-pageDiv.appendChild(newDiv);
-let refNode = document.querySelector('ul');
-refNode.after(newDiv);
+ //add ul to the "pagination" div to store pagination links
+ const linkList = document.createElement('ul');
+ linkList.classname = 'pagination';
+ newDiv.appendChild(linkList);
 
-//add ul to the "pagination" div to store pagination links
-const linkList = document.createElement('ul');
-linkList.classname = 'pagination';
-newDiv.appendChild(linkList);
-
-//for every page, add li and a tags with the page number text
-for(let i=1;i<=numOfPages;i++){
+ //for every page, add li and a tags with the page number text
+  for(let i=1;i<=numOfPages;i++){
   const li = document.createElement('li');
   if(i === 1){
   li.className = 'active';}
@@ -67,15 +63,15 @@ for(let i=1;i<=numOfPages;i++){
   showPage(studentItem,displayPage);
 
 //loop over the pagination links to remove the active class from all the links
-const pagLinks = document.querySelectorAll('li');
+  const pagLinks = document.querySelectorAll('a');
   for(let i =0;i<pagLinks.length;i++){
   pagLinks[i].classList.remove('active');
 }
 //add the active class to the link that was just clicked. Can identify that clicked link using event.target
-e.target.className = 'active';
+ e.target.className = 'active';
 
 });
 }
-
+// call functions
 showPage(studentItem,firstPage);
 appendPageLinks(studentItem);
